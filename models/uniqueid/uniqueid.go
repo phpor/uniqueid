663e101959f9718f83d648facc147757caf3ac0c
junjie.li <junjie.li@beebank.com>
2016-02-03 01:03:55 +0800

    少写了一个 i++ 调了半天

--- models/uniqueid/uniqueid.go
+++ models/uniqueid/uniqueid.go
@@ -1,10 +1,40 @@
 package uniqueid
+import (
+	"strconv"
+)
 
 type Uniqueid struct {
 
 }
 
-func Get(name string, num ...int32) (uids []string,err error) {
+var topics = []string{"test1", "test2"}
+var uniqueidChans = map[string](chan int32){}
 
+func Get(topic string, num ...int) (uids []string,err error) {
+	i := 0
+	cnt := 1
+	if len(num) > 0 {
+		cnt = num[0]
+	}
+	for i < cnt {
+		//todo: 这里需要处理chan已关闭的情况
+		uids = append(uids, strconv.Itoa(int(<-uniqueidChans[topic])))
+		i++
+	}
 	return
+}
+
+func init() {
+	for _, topic := range topics {
+		uniqueidChans[topic] = make(chan int32, 10)
+		println(topic)
+		go func(topic string) {
+			i := int32(0)
+			for {
+				println(topic, "len:" , len(uniqueidChans[topic]))
+				uniqueidChans[topic] <- i
+				i++
+			}
+		}(topic)
+	}
 }
